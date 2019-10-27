50064469171fc3003975546a7ff003a9d4cb16e5
Danica Priest <Danicamaypriest@hotmail.co.uk>
2019-10-27 20:58:49 +0000

    first test

--- src/main/java/budTest/Controller/budTestController.java
+++ src/main/java/budTest/Controller/budTestController.java
@@ -1,4 +1,20 @@
 package budTest.Controller;
 
+import budTest.Model.WorldBank;
+import budTest.Service.BudTestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/budTest")
 public class budTestController {
+    @Autowired
+    BudTestService budTestService;
+
+    @RequestMapping("/")
+    public WorldBank getCountry (@RequestParam (value="code")String code){
+        return budTestService.getCountry(code);
+    }
 }
--- src/main/java/budTest/Service/budTestService.java
+++ src/main/java/budTest/Service/budTestService.java
@@ -1,4 +1,19 @@
 package budTest.Service;
 
-public class budTestService {
+import budTest.Model.WorldBank;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class BudTestService {
+    @Autowired
+    RestTemplate restTemplate;
+
+    public WorldBank getCountry(String code){
+        String url ="http://api.worldbank.org/v2/country/" + code +"?format=json";
+        WorldBank country = restTemplate.getForObject(url, WorldBank.class);
+        return country;
+    }
+
 }
